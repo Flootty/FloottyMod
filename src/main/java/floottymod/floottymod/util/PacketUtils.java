cd09b55bb1d23380ba84a66a8e2f4efc38f45224
Flootty <konstantin.kilbel@gmail.com>
2022-11-28 20:38:28 +0100

    Added sendImmediatly for all packets

--- src/main/java/floottymod/floottymod/util/PacketUtils.java
+++ src/main/java/floottymod/floottymod/util/PacketUtils.java
@@ -3,6 +3,7 @@ package floottymod.floottymod.util;
 import floottymod.floottymod.mixin.ClientConnectionInvoker;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
+import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.math.Vec3d;
@@ -16,12 +17,6 @@ public class PacketUtils {
         conn.sendIm(new PlayerMoveC2SPacket.PositionAndOnGround(pos.x, pos.y, pos.z, false), null);
     }
 
-    public static void sendAttackEntity(Entity entity) {
-        MinecraftClient MC = MinecraftClient.getInstance();
-        ClientConnectionInvoker conn = (ClientConnectionInvoker) MC.player.networkHandler.getConnection();
-        conn.sendIm(PlayerInteractEntityC2SPacket.attack(entity, MC.player.isSneaking()), null);
-    }
-
     public static void sendRotation(float yaw, float pitch) {
         ClientConnectionInvoker conn = (ClientConnectionInvoker) MC.player.networkHandler.getConnection();
         conn.sendIm(new PlayerMoveC2SPacket.LookAndOnGround(yaw, pitch, MC.player.isOnGround()), null);
@@ -30,5 +25,10 @@ public class PacketUtils {
     public static void sendOnGround(boolean onGround) {
         ClientConnectionInvoker conn = (ClientConnectionInvoker) MC.player.networkHandler.getConnection();
         conn.sendIm(new PlayerMoveC2SPacket.OnGroundOnly(onGround), null);
+    }
+
+    public static void sendImmediatly(Packet packet) {
+        ClientConnectionInvoker conn = (ClientConnectionInvoker) MC.player.networkHandler.getConnection();
+        conn.sendIm(packet, null);
     }
 }
