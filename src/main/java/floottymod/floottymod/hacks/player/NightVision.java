35e4da4dbc57c16a11aff867a4e693fd2a84d91f
Flootty <konstantin.kilbel@gmail.com>
2022-12-02 19:52:53 +0100

    Fixed losing effect after death

--- src/main/java/floottymod/floottymod/hacks/player/NightVision.java
+++ src/main/java/floottymod/floottymod/hacks/player/NightVision.java
@@ -1,11 +1,20 @@
 package floottymod.floottymod.hacks.player;
 
+import floottymod.floottymod.events.PacketInputListener;
+import floottymod.floottymod.events.PacketOutputListener;
+import floottymod.floottymod.events.RemoveStatusEffectListener;
+import floottymod.floottymod.events.TickListener;
 import floottymod.floottymod.hack.Category;
 import floottymod.floottymod.hack.Hack;
+import floottymod.floottymod.util.ChatUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.network.packet.s2c.play.DeathMessageS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 
-public class NightVision extends Hack {
+public class NightVision extends Hack implements TickListener {
     StatusEffectInstance night_vision;
 
     public NightVision() {
@@ -17,11 +26,19 @@ public class NightVision extends Hack {
 
     @Override
     public void onEnable() {
+        EVENTS.add(TickListener.class, this);
         MC.player.addStatusEffect(night_vision);
     }
 
     @Override
     public void onDisable() {
+        EVENTS.remove(TickListener.class, this);
         MC.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+    }
+
+    @Override
+    public void onTick() {
+        StatusEffectInstance effect = MC.player.getStatusEffect(StatusEffects.NIGHT_VISION);
+        if(effect == null) MC.player.addStatusEffect(night_vision);
     }
 }
